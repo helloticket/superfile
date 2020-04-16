98216969a25d4ad29e5cb0d21bffb75c56bdb37b
Helder Guilherme Farias <helder@Helders-MacBook-Pro.local>
2020-04-16 12:19:55 -0300

    Fix

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,14 @@ import (
 	"strings"
 
 	"github.com/helderfarias/cnab-go"
+	"github.com/helderfarias/cnab-go/file"
 	"github.com/helderfarias/cnab-go/layout/itau"
-	"github.com/helderfarias/cnab-go/output"
 )
 
 func main() {
 	source := strings.NewReader(itau.CNAB240Cobranca)
 	layout, _ := cnab.NewLayout("240", source)
 	remessa := cnab.NewRemessa(layout)
-	debug := output.NewRemessaDebug(remessa)
+	debug := file.NewRemessaDebug(remessa)
 	debug.Write()
 }
